93bdbf8515c3e5298482e83325590fb195b955b7
Marre3 <marre244@gmail.com>
2022-09-13 01:24:15 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,6 +6,10 @@ input.onButtonPressed(Button.A, function () {
         Client_ID = 1
         radio.sendNumber(Client_ID)
         sentClientID = true
+    } else if (selectedWaterAmount > 0) {
+        selectedWaterAmount += -1
+    } else {
+    	
     }
 })
 input.onButtonPressed(Button.B, function () {
@@ -13,9 +17,14 @@ input.onButtonPressed(Button.B, function () {
         Client_ID = 2
         radio.sendNumber(Client_ID)
         sentClientID = true
+    } else if (selectedWaterAmount < 25) {
+        selectedWaterAmount += 1
+    } else {
+    	
     }
 })
 let Client_ID = 0
+let selectedWaterAmount = 0
 let sentClientID = false
 sentClientID = false
 radio.setGroup(137)
@@ -23,6 +32,7 @@ basic.pause(100)
 radio.sendString("")
 basic.pause(100)
 let waterCount = 25
+selectedWaterAmount = 5
 basic.forever(function () {
 	
 })
